101493c149e3e36a7b90baf5f448c4bd9d41228b
John Son <40287191+son7211@users.noreply.github.com>
2020-12-11 18:01:13 +0900

    Create invite.js

---
+++ invite.js
@@ -0,0 +1,18 @@
+const { Octokit } = require("@octokit/rest");
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+const octokit = new Octokit({
+    auth: core.getInput('github_token'),
+    userAgent: 'myApp v0.0.1',
+    baseUrl: 'https://api.github.com'
+    });
+
+await octokit.request('PUT /repos/{owner}/{repo}/collaborators/{username}', {
+  owner: 'org-name',
+  repo: 'repo-name',
+  teamname: 'team-name',
+  permission: 'push'
+})
+
+# permission can be one of : pull, push, admin, maintain, triage
